8e1186f7cfd8d86d3cdfe210704582b6d9c05f59
mirrox52 <mirrox@tut.by>
2020-02-25 03:41:27 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,35 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) 
+    {
+        throw new Error('array doesn\'t exist');
+    }
+    let res = [];
+    for (let i = 0; i < arr.length; i++)
+    {
+        switch (arr[i])
+        {
+            case '--discard-next':
+                i++;
+                break;
+            case '--discard-prev':
+                res.pop();
+                break;
+            case '--double-next':
+                if (i !== arr.length - 1)
+                {
+                    res.push(arr[i+1]);
+                }
+                break;
+            case '--double-prev':
+                if (i > 0)
+                {
+                    res.push(arr[i - 1]);
+                }
+                break;
+            default:
+                res.push(arr[i]);
+                break;
+        }
+    }
+    return res;
 };
